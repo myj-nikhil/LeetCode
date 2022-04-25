ae741e401fba7d2d38f2c652b1bc4de47e401f41
Nikhil <32569883+myj-nikhil@users.noreply.github.com>
2022-04-25 21:58:19 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (63.06%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] plusOne(int[] a) {
         int n = a.length;
-        int carry =1;
+        // int carry =1;
         int[] newa = new int[n+1];
         for(int i=0;i<n;i++) {
-            if(a[n-1-i]+carry < 10) {
+            if(a[n-1-i]+1 < 10) {
                 a[n-1-i] +=1;
                 // System.out.println("i = " +i);
                 return a;
             }
             else{
-                a[n-1-i]=a[n-1-i] +carry-10;
+                a[n-1-i]=a[n-1-i] -9;
             }
         }
         if(a[0]==0){
